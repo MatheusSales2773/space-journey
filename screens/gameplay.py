0467519c2589b698cd42c89fa6dd6995911f5ed1
pablodixs <pabllosoarez@gmail.com>
2025-04-29 18:16:54 -0300

    refactor: add images directory in assets and change imports

--- screens/gameplay.py
+++ screens/gameplay.py
@@ -7,8 +7,8 @@ class GameplayState(State):
     def __init__(self):
         super().__init__()
         self.font = pygame.font.SysFont(None, 48)
-        self.spaceship_image = pygame.image.load("assets/spaceship.png").convert_alpha()
-        self.bullet_image = pygame.image.load("assets/bullet.png").convert_alpha()
+        self.spaceship_image = pygame.image.load("assets/images/spaceship.png").convert_alpha()
+        self.bullet_image = pygame.image.load("assets/images/bullet.png").convert_alpha()
 
         self.spaceship = Spaceship(self.spaceship_image, (400, 500))
         self.bullets = pygame.sprite.Group()
