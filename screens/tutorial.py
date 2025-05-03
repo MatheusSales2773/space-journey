2a15253f863626642790177ba0663e75ae2469f3
Pablo Dias <pabllosoarez@gmail.com>
2025-05-03 14:25:14 -0300

    feat: add the spaceship image on tutorial screen

--- screens/tutorial.py
+++ screens/tutorial.py
@@ -7,10 +7,17 @@ class TutorialState(State):
     def __init__(self, manager):
         super().__init__()
         self.manager      = manager
-        self.scroll_speed = 150
+        self.scroll_speed = 250
+        self.is_scrolling = False
+
+        screen_w, screen_h = pygame.display.get_surface().get_size()
+
+        # Spaceship
+        initial_position = (screen_w // 2 - 70, screen_h // 2 + 100)
+        self.spaceship = pygame.image.load('assets/images/spaceship_static.png').convert_alpha()
+        self.rect = self.spaceship.get_rect(center=initial_position)
 
         # Carrega e escala a imagem para preencher horizontalmente
-        screen_w, screen_h = pygame.display.get_surface().get_size()
         img = pygame.image.load(
             "assets/images/backgrounds/earth_lift_off_bg.png"
         ).convert()
@@ -26,31 +33,35 @@ class TutorialState(State):
         # Começa mostrando o final da imagem (offset máximo)
         self.y_offset   = self.max_offset
 
-        # Fonte e mensagem
         self.font = pygame.font.Font(settings.FONT_PATH, settings.FONT_SIZE_GAME)
-        self.msg  = "Pressione ENTER para começar"
+
+        self.msg = "Pressione ESPAÇO para iniciar o scroll"
+        self.start_msg = "Pressione ENTER para começar"
+
 
     def handle_events(self, events):
         for e in events:
-            if e.type == pygame.KEYDOWN and e.key in (pygame.K_RETURN, pygame.K_ESCAPE):
-                # Avança para o jogo sem reiniciar estado
-                self.manager.set_state(GameplayState(self.manager))
+            if e.type == pygame.KEYDOWN:
+                if e.key == pygame.K_SPACE and not self.is_scrolling:
+                    self.is_scrolling = True
+                    self.msg = self.start_msg
+                elif e.key in (pygame.K_RETURN, pygame.K_ESCAPE) and self.y_offset == 0:
+                    self.manager.set_state(GameplayState(self.manager))
 
     def update(self, dt):
-        # Move a imagem para cima até y_offset = 0
-        if self.y_offset > 0:
+        if self.is_scrolling and self.y_offset > 0:
             self.y_offset -= self.scroll_speed * dt
             if self.y_offset < 0:
                 self.y_offset = 0
 
     def draw(self, screen):
-        # Desenha a parte correta da imagem
         screen.blit(self.tutorial_img, (0, -int(self.y_offset)))
 
-        # Quando chegar ao topo, exibe o prompt
         if self.y_offset == 0:
             text_surf = self.font.render(self.msg, True, (255, 255, 255))
             text_rect = text_surf.get_rect(
                 midbottom=(screen.get_width() // 2, self.screen_h - 20)
             )
             screen.blit(text_surf, text_rect)
+
+        screen.blit(self.spaceship, self.rect)
